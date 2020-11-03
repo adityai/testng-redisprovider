a1fc96de86fda0fbf3103e6408791f6500493025
Inapurapu, Aditya <aditya.inapurapu@blueshieldca.com>
2020-11-03 10:02:42 -0800

    Proper redis provider implemented

--- src/test/java/com/iaditya/testng/sqliteprovider/NewTest.java
+++ src/test/java/com/iaditya/testng/sqliteprovider/NewTest.java
@@ -6,12 +6,16 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import redis.clients.jedis.Jedis;
 
 /**
  * Sample test class to demonstrate use of sqlite as the data source of the test data.
@@ -28,7 +32,7 @@ public class NewTest {
 	 * @param a
 	 * @param b
 	 */
-  @Test(dataProvider="sqliteDataProvider")
+  @Test(dataProvider="redisDataProvider")
   public void testCase001(String a, String b) {
 	  Assert.assertEquals(a, "testData_001_1");
 	  Assert.assertEquals(b, "testData_001_2");
@@ -40,94 +44,50 @@ public class NewTest {
    * @param a
    * @param b
    */
-  @Test(dataProvider="sqliteDataProvider")
+  @Test(dataProvider="redisDataProvider")
   public void testCase002(String a, String b) {
 	  Assert.assertEquals(a, "testData_002_1");
 	  Assert.assertEquals(b, "testData_002_2");
   }
   
+  @AfterSuite
+  private void cleanUp() {
+	  Jedis jedis = new Jedis("0.0.0.0", 6379);
+	  jedis.connect();
+	  jedis.del("testCase001");
+	  jedis.del("testCase002");
+	  jedis.close();
+  }
+  
 
   /**
-   * Before suite, create sqlite database instance and insert data
+   * Before suite, create redis database instance and insert data
    */
   @BeforeSuite
-  private void initSqlite() {
-		    Connection connection = null;
-            Statement statement = null;
-            String createTableSql = "CREATE TABLE IF NOT EXISTS testdata ("
-                    + "	TestCaseName text PRIMARY KEY,"
-                    + "	TestMethod text NOT NULL,"
-                    + "	data1 text,"
-                    + "	data2 text"
-                    + ")";    
-            String url = "jdbc:sqlite:src/test/resources/testdata.db";
-            try {
-				connection = DriverManager.getConnection(url);
-	            System.out.println("Connection to SQLite has been established.");
-				statement = connection.createStatement();
-				statement.execute(createTableSql);
-				statement.executeUpdate("delete from testdata;");
-				statement.executeUpdate("INSERT INTO testdata VALUES ('TEST_CASE_001', 'testCase001', 'testData_001_1', 'testData_001_2' );");
-				statement.executeUpdate("INSERT INTO testdata VALUES ('TEST_CASE_002', 'testCase002', 'testData_002_1', 'testData_002_2' );");
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-	            System.out.println("Connection to SQLite has NOT been established.");
-			} finally {
-				try {
-				    statement.close();
-				    connection.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+  private void initRedis() {
+	  Jedis jedis = new Jedis("0.0.0.0", 6379);
+	  jedis.connect();
+	  Map<String, String> hash = new HashMap<String, String>();
+	  hash.put("key1", "testData_001_1");
+	  hash.put("key2", "testData_001_2");
+	  jedis.hset("testCase001", hash);
+	  
+	  hash = new HashMap<String, String>();
+	  hash.put("key1", "testData_002_1");
+	  hash.put("key2", "testData_002_2");
+	  jedis.hset("testCase002", hash);
+	  jedis.close();
+	  
   }
-
-  /**
-   * After test suite, stop sqlite
-   */
-  @AfterSuite
-  private void stopSqlite(){
-
-  }
-
-  /**
-   * Dataprovider for reading data from sqlite database
-   * 
-   * @param method
-   * @return object array with test data
-   */
-  @DataProvider(name="sqliteDataProvider")
+  
+  @DataProvider(name="redisDataProvider")
   private Object[][] getData(Method method) {
-	    Object[][] data = null;
-	    Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
-        
-        String url = "jdbc:sqlite:src/test/resources/testdata.db";
-        try {
-			connection = DriverManager.getConnection(url);
-            System.out.println("Connection to SQLite has been established.");
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery("select * from testdata where TestMethod = '" + method.getName() + "'");
-			resultSet.next();
-			data = new Object[][] {{ resultSet.getString("data1"), resultSet.getString("data2") }};
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-            System.out.println("Connection to SQLite has NOT been established.");
-		} finally {
-			try {
-				resultSet.close();
-			    statement.close();
-			    connection.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	    
+	  Object[][] data = null;
+	  Jedis jedis = new Jedis("0.0.0.0", 6379);
+	  jedis.connect();
+	  Map<String, String> testCaseMap = jedis.hgetAll(method.getName());
+	  data = new Object[][] {{ testCaseMap.get("key1"), testCaseMap.get("key2") }};
+	  jedis.close();
 	  return data;
   }
   
